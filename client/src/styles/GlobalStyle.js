7520c9e598aa2658e0a6e6168809771b8a269c58
seoJunPyo <cooljp95@naver.com>
2023-04-17 21:41:12 +0900

    [STYLE] Global style 설정
    Co-authored-by: kyle kwon <kylekwon96@gmail.com>

---
+++ client/src/styles/GlobalStyle.js
@@ -0,0 +1,28 @@
+import { css } from '@emotion/react';
+
+const GlobalStyle = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+    font-family: 'Noto Sans KR', sans-serif;
+    background-color: #f5f5f7;
+  }
+
+  body[data-theme='light'] {
+    --font-color: #000;
+    --body-bg-color: #fff;
+  }
+
+  body[data-theme='dark'] {
+    --font-color: #fff;
+    --body-bg-color: #232323;
+  }
+`;
+
+export default GlobalStyle;
