2ee3aca45a579d9c6385ae894eea97784365287e
seoJunPyo <cooljp95@naver.com>
2023-04-19 21:39:05 +0900

    [FEAT] SignUp 페이지 / auth 컴포넌트 index 추가

---
+++ client/src/components/auth/index.js
@@ -0,0 +1,6 @@
+export { default as CountrySelect } from './CountrySelect';
+export { default as FormInput } from './FormInput';
+export { default as PasswordInput } from './PasswordInput';
+export { default as BirthDateInput } from './BirthDateInput';
+export { default as Input } from './Input';
+export { default as EmailInput } from './EmailInput';
