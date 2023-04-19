3e3baefe765dcd04cedb5b6d55910c96e867afae
sunhwa <sumz.ko@gmail.com>
2023-04-19 20:48:39 +0900

    [FEAT] Header 컴포넌트

---
+++ client/src/components/common/Header.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Container } from '@mantine/core';
+import { Link } from 'react-router-dom';
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  border-bottom: 1px solid #cdcdcd;
+  background-color: var(--body-bg-color);
+  position: sticky;
+  top: 0;
+`;
+
+const Wrapper = styled(Container)`
+  min-width: 1024px;
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.75rem;
+  padding: 15px 0px 10px;
+`;
+
+const Title = styled(Link)`
+  font-size: 21px;
+  font-weight: 600;
+  color: var(--font-color);
+  text-decoration: none;
+
+  :hover {
+    color: var(--hover-font-color);
+  }
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  gap: 30px;
+  margin: auto 0;
+`;
+
+const Menu = styled(Title)`
+  font-size: 14px;
+  font-weight: 400;
+`;
+
+/**
+ * - `title` : ㅇㄴㄹㄴㅇㄹ
+ * - `menu` : asdfsd
+ * @param {{
+ * title: {path: string, content: string}
+ * menuList: Array<{path: string, content: string}>
+ * }} props
+ */
+const Header = ({ title, menuList }) => (
+  <HeaderContainer>
+    <Wrapper>
+      <Title to={title.path}>{title.content}</Title>
+      <MenuWrapper>
+        {menuList.map(({ path, content }) => (
+          <Menu to={path} key={content}>
+            {content}
+          </Menu>
+        ))}
+      </MenuWrapper>
+    </Wrapper>
+  </HeaderContainer>
+);
+
+export default Header;
