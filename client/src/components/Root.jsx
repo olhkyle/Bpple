b8e2b0b58191d0eb3c0e68ce718cbf629e781163
olhkyle <kylekwon96@gmail.com>
2023-04-19 16:51:30 +0900

    [FEAT] : Root 컴포넌트 내 Footer 컴포넌트 추가

--- client/src/components/Root.jsx
+++ client/src/components/Root.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useLocation, Outlet } from 'react-router-dom';
 import routesConstants from '../constants/routes';
+import Footer from './common/Footer';
 
 const Root = () => {
   const { pathname } = useLocation();
 
-  return <div>{pathname === routesConstants.MAIN ? <div>Main</div> : <Outlet />}</div>;
+  return (
+    <>
+      <div>{pathname === routesConstants.MAIN ? <div>Main</div> : <Outlet />}</div>
+      <Footer />
+    </>
+  );
 };
 
 export default Root;
