1851eb94fd828b5393af105143595ca4fb17a5a0
sunhwa <sumz.ko@gmail.com>
2023-04-20 15:09:11 +0900

    [-] PopupModal - index 추가

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,3 +1,5 @@
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as FormInput } from './FormInput';
+export { default as PopupModal } from './PopupModal';
+export { default as TextCard } from './TextCard';
