9d3b16b31a8261a0b8e4b4e40bd42eb530a9b8df
sunhwa <sumz.ko@gmail.com>
2023-04-20 20:08:33 +0900

    AvatarCard 컴포넌트

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -3,3 +3,4 @@ export { default as Header } from './Header';
 export { default as FormInput } from './FormInput';
 export { default as PopupModal } from './PopupModal';
 export { default as TextCard } from './TextCard';
+export { default as AvatarCard } from './AvatarCard';
