d443a72696c87dc0f2b84bfc98c8f19514029013
seoJunPyo <cooljp95@naver.com>
2023-04-20 12:28:56 +0900

    [REACTOR] SignIn 컴포넌트 분리 / SignIn Page 수정

---
+++ client/src/components/auth/SignIn/SignInForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Title, Stack } from '@mantine/core';
+import { EmailInput, PasswordInput } from '.';
+
+const signinScheme = z.object({
+  email: z.string().email({ message: '이메일 형식에 맞게 입력해 주세요.' }),
+  password: z.string().regex(/^[0-9a-zA-Z]{6,12}$/, { message: '영문 또는 숫자를 6~12자 입력하세요.' }),
+});
+
+const SignInForm = () => {
+  // TODO : trigger 디바운스 하기
+  const { handleSubmit, control, trigger, setValue } = useForm({ resolver: zodResolver(signinScheme) });
+  const [emailPassed, setEmailPassed] = React.useState(false);
+  const [toolTipOpened, setToolTipOpened] = React.useState(false);
+
+  const onSubmit = async data => {
+    try {
+      const res = await axios.post('api/signin', data);
+      // TODO : MAIN으로 이동 , 유저 정보 전역 상태로 관리하기
+      console.log(res.data);
+    } catch (e) {
+      setValue('password', '');
+      setToolTipOpened(true);
+    }
+  };
+
+  return (
+    <form>
+      <Stack h="300px" justify="center" align="center">
+        <Title c="#494949" fz="22px" order={2}>
+          Apple Store에 로그인하세요
+        </Title>
+        <Stack w="340px" spacing="0">
+          <EmailInput
+            control={control}
+            trigger={trigger}
+            emailPassed={emailPassed}
+            setEmailPassed={setEmailPassed}
+            closeTooltip={() => setToolTipOpened(false)}
+          />
+          {emailPassed && (
+            <PasswordInput
+              toolTipOpened={toolTipOpened}
+              closeTooltip={() => setToolTipOpened(false)}
+              control={control}
+              trigger={trigger}
+              subMit={handleSubmit(onSubmit)}
+            />
+          )}
+        </Stack>
+      </Stack>
+    </form>
+  );
+};
+
+export default SignInForm;
---
+++ client/src/components/auth/SignIn/SingIn.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import { Container, Title, Text, Stack } from '@mantine/core';
+import { WiDirectionUpRight } from 'react-icons/wi';
+import { SignInForm } from '.';
+
+const LinkBox = styled.span`
+  display: inline-block;
+  transform: translateY(8px);
+`;
+
+const SingIn = () => (
+  <Container>
+    <Title>더욱 빠르게 결제하시려면 로그인하세요.</Title>
+    <Stack align="center">
+      <SignInForm />
+      <Text fz="14px">
+        Apple ID가 없으십니까?
+        {/** TODO : SINGUP 페이지로 이동시키기 */}
+        <Link>
+          지금 만드세요.
+          <LinkBox>
+            <WiDirectionUpRight size="24" />
+          </LinkBox>
+        </Link>
+      </Text>
+    </Stack>
+  </Container>
+);
+
+export default SingIn;
