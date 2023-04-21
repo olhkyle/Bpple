ffda3abef89364f54cc7ac20aac2150a9b0a17e1
olhkyle <kylekwon96@gmail.com>
2023-04-21 12:52:19 +0900

    [STYLE] : InfoCard 컴포넌트 수정

--- client/src/components/common/InfoCard.jsx
+++ client/src/components/common/InfoCard.jsx
@@ -1,42 +1,43 @@
 import React from 'react';
-import { Stack, Center, Text } from '@mantine/core';
 import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
+import { Stack, Center, Text, Button } from '@mantine/core';
+import { RiArrowRightSLine } from 'react-icons/ri';
 
 const Icon = styled.div`
+  color: var(--font-color);
   font-size: 48px;
 `;
 
-const TextMore = styled(Text)`
-  ::after {
-    content: '>';
-    display: inline-block;
-    transform: translate3d(15%, 0, 0);
-  }
+const RightArrowIcon = styled(RiArrowRightSLine)`
+  padding-top: 1px;
+  font-size: 21px;
 `;
 
 /**
  * icon : 랜더링할 아이콘
  * title : bold체로 표현되는 Text
  * desc : Info 설명
- * href : 더 알아보기 클릭 시, 이동할 링크
+ * onClick : 더 알아보기 클릭 시 Modal 띄우기
  * @param {{icon, title, desc, href}} param0
  * @returns JSX
  */
 
-const InfoCard = ({ icon, title, desc, href }) => (
-  <Center sx={{ textAlign: 'center' }}>
+const InfoCard = ({ icon, title, desc, onClick }) => (
+  <Center sx={{ textAlign: 'center', wordBreak: 'keep-all' }}>
     <Stack align="center">
       <Icon>{icon}</Icon>
       <Text fz="28px" fw="600">
         {title}
       </Text>
-      <Text w={'60%'} sx={{ wordBreak: 'keep-all' }}>
-        {desc}
-      </Text>
-      <Link to={href}>
-        <TextMore>더 알아보기</TextMore>
-      </Link>
+      <Text w={'60%'}>{desc}</Text>
+      <Button
+        radius="lg"
+        pr="12px"
+        onClick={onClick}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Text>더 알아보기</Text>
+        <RightArrowIcon />
+      </Button>
     </Stack>
   </Center>
 );
