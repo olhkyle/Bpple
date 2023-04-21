a6516d00f40d32f597e0a7a0ff4f21284a3f351c
sunhwa <sumz.ko@gmail.com>
2023-04-21 20:06:14 +0900

    [FEAT] Nav - Cart 버튼, Theme 버튼, 로그인 버튼 추가

--- client/src/components/common/nav/Nav.jsx
+++ client/src/components/common/nav/Nav.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Header, Container, Group, Menu, Text } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import { BsCart } from 'react-icons/bs';
+import routesConstants from '../../../constants/routes';
 import MacSubMenu from './MacSubMenu';
 import IphoneSubMenu from './IphoneSubMenu';
 import CommunitySubMenu from './CommunitySubMenu';
+import ThemeButton from './ThemeButton';
+import LoginButton from './LoginButton';
 
 const NavContainer = styled(Header)`
   display: flex;
@@ -15,10 +20,14 @@ const NavContainer = styled(Header)`
 
 const Wrapper = styled(Group)`
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  height: 100%;
   gap: 20px;
+`;
+
+const SideWrapper = styled(Wrapper)`
+  margin-right: 30px;
 `;
 
 const TextMenu = styled(Menu)`
@@ -29,10 +38,25 @@ const NavItem = styled(Text)`
   font-size: 20px;
   font-weight: 500;
   padding: 8px 15px;
-  &[aria-expanded='true'] {
+  :hover {
     color: var(--hover-font-color);
+  }
+  &[aria-expanded='true'] {
     cursor: default;
   }
+`;
+
+const CartLink = styled(Link)`
+  display: flex;
+  padding: 8px;
+  color: var(--font-color);
+  :hover {
+    color: var(--hover-font-color);
+  }
+`;
+
+const IconLink = styled.i`
+  font-size: 24px;
 `;
 
 const Nav = () => (
@@ -62,6 +86,16 @@ const Nav = () => (
           </TextMenu>
         </Wrapper>
       </Container>
+
+      <SideWrapper>
+        <CartLink to={routesConstants.CART}>
+          <IconLink>
+            <BsCart />
+          </IconLink>
+        </CartLink>
+        <ThemeButton />
+        <LoginButton />
+      </SideWrapper>
     </NavContainer>
   </>
 );
