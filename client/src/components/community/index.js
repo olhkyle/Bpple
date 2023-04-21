b98d29943661e4ac0869c4a86a2600eba6e23de0
olhkyle <kylekwon96@gmail.com>
2023-04-21 20:43:30 +0900

    [-] : index.js community 관련 컴포넌트 추가

--- client/src/components/community/index.js
+++ client/src/components/community/index.js
@@ -2,4 +2,7 @@ export { default as CommunityHeader } from './CommunityHeader';
 export { default as CommunityMain } from './CommunityMain';
 export { default as CommunityMyPosts } from './CommunityMyPosts';
 export { default as CommunityPosts } from './CommunityPosts';
+export { default as AutoComplete } from './AutoComplete';
+export { default as QuestionModal } from './QuestionModal';
+export { default as FilterContentModal } from './FilterContentModal';
 export { default as RuleModal } from './RuleModal';
