39cbf494b558f3b28eef1a69d03d01e27c2e362f
olhkyle <kylekwon96@gmail.com>
2023-04-23 18:57:51 +0900

    [-] : CommunityMain import 경로 수정

--- client/src/components/community/CommunityMain.jsx
+++ client/src/components/community/CommunityMain.jsx
@@ -6,11 +6,8 @@ import { IoFilterCircleOutline } from 'react-icons/io5';
 import { HiOutlineTrophy } from 'react-icons/hi2';
 import { Container, Divider, Flex, Group, List, Text, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import InfoCard from '../common/InfoCard';
-import AutoComplete from './AutoComplete';
-import RuleModal from './RuleModal';
-import QuestionModal from './QuestionModal';
-import FilterContentModal from './FilterContentModal';
+import { InfoCard } from '../common';
+import { AutoComplete, RuleModal, QuestionModal, FilterContentModal } from '.';
 
 const Wrapper = styled(Container)`
   min-width: 1024px;
@@ -86,78 +83,8 @@ const categoryList = [
   { imgPath: '/community/ipad-family.png', category: 'iPad' },
 ];
 
-const posts = [
-  {
-    id: 1,
-    title: '새로운 icloud 이용 약관이 설정창에 떠서 동의를 눌렀음에도 안 없어지고 계속 떠있습니다. 해결 방법 있을까요?',
-    author: 'jp',
-    createdAt: '2023-02-23',
-    updatedAt: '2023-03-01',
-    comments: [1, 2, 3, 4, 5, 6],
-    category: 'iphone',
-    completed: false,
-  },
-  {
-    id: 2,
-    title: '집중모드 위치설정 기기변경을 하려하는데 예전 폰으로 위치 설정이 잡히는데 어떻게 하는게 좋을까요?',
-    author: 'jp',
-    createdAt: '2023-02-20',
-    updatedAt: '2023-02-24',
-    comments: [1, 2, 3, 4, 5, 6],
-    category: 'iphone',
-    completed: true,
-  },
-  {
-    id: 3,
-    title: '특정 어플을 들어갈때마다 `java.lang.String` 관련 오류가 뜨는데 해결책이 있을까요?',
-    author: 'jp',
-    createdAt: '2023-02-10',
-    updatedAt: '2023-02-26',
-    comments: [1, 2, 3, 4, 5, 6],
-    category: 'mac',
-    completed: false,
-  },
-  {
-    id: 4,
-    title: '아이폰 너무해?',
-    author: 'jp',
-    createdAt: '2023-02-05',
-    updatedAt: '2023-02-26',
-    comments: [1, 2, 3, 4, 5, 6],
-    category: 'iphone',
-    completed: true,
-  },
-  {
-    id: 5,
-    title:
-      '아이폰 13 미니에서 찍은 영상 에어드롭으로 아이폰7+로 보냈을때 영상에 빛노출이 너무 심한데 어떻게 해야 하나요?',
-    author: 'jp',
-    createdAt: '2023-01-15',
-    updatedAt: '2023-02-26',
-    comments: [1, 2, 3, 4, 5, 6],
-    category: 'mac',
-    completed: false,
-  },
-  {
-    id: 6,
-    title: '아이폰 13 미니 에서 블루투스 인지를 못하는 이유?',
-    author: 'jp',
-    createdAt: '2023-02-20',
-    updatedAt: '2023-02-26',
-    comments: [1, 2, 3, 4, 5, 6],
-    category: 'ipad',
-    completed: true,
-  },
-];
-
-const filteredPosts = posts.map(({ id, title }) => ({ title, value: id }));
-
 // Todo []
-// 1. 게시글 목록 get요청 필요
-// 2. input dropdown 서버 요청에 따른 loading spinner 처리
-// 3. Link 요소 활용하여 해당 게시글 링크로 라우팅 처리
-// 4. 카테고리별 페이지로 이동 필요
-// sub - 5. autocomplete dropdown tab 이동 구현
+// 1. 카테고리별 페이지로 이동 필요
 
 const CommunityMain = () => {
   const [questionModalOpened, { close: onQuestionModalClose, open: onQuestionModalOpen }] = useDisclosure(false);
@@ -174,7 +101,8 @@ const CommunityMain = () => {
         <Text fz="26px" mb="40px">
           전 세계 FineApple 고객들과 소통해 보세요 🚀
         </Text>
-        <AutoComplete communityPosts={filteredPosts} width={720} />
+
+        <AutoComplete width={720} />
       </Description>
 
       <CommunityImageWrapper>
@@ -188,7 +116,7 @@ const CommunityMain = () => {
       <CategoryList>
         {categoryList.map(({ imgPath, category }) => (
           <Category key={imgPath}>
-            <Link to={''}>
+            <Link to={`/community/${category.toLowerCase()}`}>
               <img src={imgPath} alt="iphone-category" />
               <CategoryDescription>{category}</CategoryDescription>
             </Link>
