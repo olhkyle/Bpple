1cc94bc533fe9922bc040167561cba30d915cbc4
olhkyle <kylekwon96@gmail.com>
2023-04-23 18:56:49 +0900

    [-] : index.js InfoCard 컴포넌트 추가

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -5,3 +5,4 @@ export { default as FormInput } from './FormInput';
 export { default as PopupModal } from './PopupModal';
 export { default as TextCard } from './TextCard';
 export { default as AvatarCard } from './AvatarCard';
+export { default as InfoCard } from './InfoCard';
