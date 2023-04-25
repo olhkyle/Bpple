3dfbeaf41b0214381b0f76f072333e28b735b609
sunhwa <sumz.ko@gmail.com>
2023-04-25 10:44:19 +0900

    [FEAT] AvatarButton - hover ui 변경

--- client/src/components/profile/avatar/AvatarButton.jsx
+++ client/src/components/profile/avatar/AvatarButton.jsx
@@ -1,21 +1,46 @@
 import styled from '@emotion/styled';
-import { Avatar } from '@mantine/core';
+import { Avatar, Container } from '@mantine/core';
 import React from 'react';
 import avatars from '../../../constants/avatars';
 
-const Container = styled(Avatar)`
+const AvatarContainer = styled(Container)`
+  position: relative;
+  padding: 0;
+`;
+
+const AvatarWrapper = styled(Avatar)`
   background-color: #d1d1d1;
   border: 3px solid #d1d1d1;
   border-radius: 100%;
+`;
+
+const Overlay = styled(Container)`
+  position: absolute;
+  cursor: pointer;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.8rem;
+  color: #e0e0e0;
 
   :hover {
-    cursor: ${({ clickable }) => clickable && 'pointer'};
-    background-color: ${({ selectable }) => selectable && `var(--hover-font-color)`};
+    background-color: rgba(0, 0, 0, 0.7);
+    :after {
+      content: 'Edit';
+    }
   }
 `;
 
 const AvatarButton = ({ avatarId, onClick }) => (
-  <Container src={avatars[avatarId]} onClick={onClick} clickable={!!onClick} alt="avatar image button" />
+  <AvatarContainer>
+    <AvatarWrapper size="xl" src={avatars[avatarId]} alt="avatar image button" />
+    {onClick && <Overlay onClick={onClick} />}
+  </AvatarContainer>
 );
 
 export default AvatarButton;
