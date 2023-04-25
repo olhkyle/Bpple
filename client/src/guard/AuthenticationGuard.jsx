1e899f1c0c0652778f9c80449f6baa0d697796d4
sunhwa <sumz.ko@gmail.com>
2023-04-25 20:29:31 +0900

    [FEAT] AuthenticationGuard error -  userState 전역 상태 변경

--- client/src/guard/AuthenticationGuard.jsx
+++ client/src/guard/AuthenticationGuard.jsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import Recoil from 'recoil';
 import { useQuery } from '@tanstack/react-query';
 import { Navigate } from 'react-router-dom';
 import { auth } from '../api/auth';
+import userState from '../recoil/atoms/userState';
 
 /**
  * Protected Route
@@ -12,6 +15,14 @@ const AuthenticationGuard = ({ redirectTo, element }) => {
     retry: false,
     staleTime: 1000,
   });
+
+  const setLoginUser = Recoil.useSetRecoilState(userState);
+
+  React.useEffect(() => {
+    if (isFetched && error) {
+      setLoginUser(null);
+    }
+  }, [error, isFetched, setLoginUser]);
 
   return isFetched ? error === null ? element : <Navigate to={redirectTo} /> : null;
 };
