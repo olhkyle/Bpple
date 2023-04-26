8182de155303c13ff25f2c6f5b278a4ba6921bb8
olhkyle <kylekwon96@gmail.com>
2023-04-26 11:05:01 +0900

    [-] : FormInput 컴포넌트 제거로 export 문 제거

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,7 +1,6 @@
 export { default as Logo } from './Logo';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
-export { default as FormInput } from './FormInput';
 export { default as PopupModal } from './PopupModal';
 export { default as TextCard } from './TextCard';
 export { default as AvatarCard } from './AvatarCard';
