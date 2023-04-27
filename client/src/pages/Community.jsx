fb47d404c190bc3ce398d73cd47983df89d30ab1
olhkyle <kylekwon96@gmail.com>
2023-04-27 10:20:24 +0900

    [REFAC] : Community 페이지 컴포넌트 Suspense 제거

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -3,7 +3,6 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { COMMUNITY_PATH } from '../routes/routePaths';
 import { CommunityHeader } from '../components/community';
 import CommunityMain from '../components/community/CommunityMain';
-import Loader from '../components/common/Loader';
 
 const Community = () => {
   const { pathname } = useLocation();
@@ -11,9 +10,7 @@ const Community = () => {
   return (
     <>
       <CommunityHeader />
-      <React.Suspense fallback={<Loader />}>
-        {pathname === COMMUNITY_PATH ? <CommunityMain /> : <Outlet />}
-      </React.Suspense>
+      {pathname === COMMUNITY_PATH ? <CommunityMain /> : <Outlet />}
     </>
   );
 };
