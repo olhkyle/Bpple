ca154e594d08eae3a57a1187d7d156c79cb92b2d
sunhwa <sumz.ko@gmail.com>
2023-04-27 16:31:36 +0900

    [SERVER] router - rank

--- server/router/rank.js
+++ server/router/rank.js
@@ -4,9 +4,10 @@ const users = require('../mock-data/users');
 
 // 사용자 랭킹
 router.get('/', (req, res) => {
-	const rankList = users.getUsersRank();
+	const { topCount } = req.query;
+	const usersRank = users.getUsersRank(topCount);
 
-	res.send({ rankList });
+	res.send({ usersRank });
 });
 
 module.exports = router;
