ce6c8c6600631449944258c0255eedc56f2da696
olhkyle <kylekwon96@gmail.com>
2023-04-28 21:43:11 +0900

    [FEAT] : comment CRUD 관련 mutation 구현

---
+++ client/src/hooks/mutations/useCommentMutation.js
@@ -0,0 +1,30 @@
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+
+const keyword = 'comments';
+
+const useCommentMutation = ({ requestFn, queryKeyword = keyword, postId, onMutate: expected, ...options }) => {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: async variables => {
+      await requestFn({ postId, ...variables });
+    },
+    onMutate: async variables => {
+      await queryClient.cancelQueries({ queryKey: [queryKeyword, postId] });
+
+      const prevComments = queryClient.getQueryData([queryKeyword, postId]);
+
+      queryClient.setQueryData([queryKeyword, postId], oldData => expected(oldData, variables));
+
+      return { prevComments };
+    },
+    onError: (error, variables, { prevComments }) => {
+      queryClient.setQueryData([queryKeyword, postId], prevComments);
+    },
+    ...options,
+  });
+
+  return mutate;
+};
+
+export default useCommentMutation;
