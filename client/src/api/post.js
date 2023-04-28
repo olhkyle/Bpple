d52336c48572ad02bd6dc31a13a6a8a90f7e4562
olhkyle <kylekwon96@gmail.com>
2023-04-28 21:47:02 +0900

    [FEAT] : post, posts 관련 비동기 요청 query문 추가

--- client/src/api/post.js
+++ client/src/api/post.js
@@ -1,7 +1,21 @@
 import axios from 'axios';
 
+const getPost = postId => axios.get(`/api/post/${postId}`);
+
 const createNewPost = postInfo => axios.post('/api/post', { postInfo });
 
 const updatePost = (postInfo, postId) => axios.patch(`/api/post/${postId}`, { postInfo });
 
-export { createNewPost, updatePost };
+const addComment = ({ postId, commentInfo }) => axios.post(`/api/post/${postId}/comment`, { commentInfo });
+
+const editComment = ({ postId, commentId, commentInfo }) =>
+  axios.patch(`/api/post/${postId}/comment/${commentId}`, { commentInfo });
+
+const removeComment = ({ postId, commentId }) => axios.delete(`/api/post/${postId}/comment/${commentId}`);
+
+const getComments = ({ param: postId, pageParam }) => axios.get(`/api/post/${postId}/comment?page=${pageParam}`);
+
+const checkIsUseFul = ({ postId, commentId, useful }) =>
+  axios.patch(`/api/post/${postId}/comment/useful/${commentId}`, { useful });
+
+export { getPost, createNewPost, updatePost, addComment, editComment, removeComment, getComments, checkIsUseFul };
