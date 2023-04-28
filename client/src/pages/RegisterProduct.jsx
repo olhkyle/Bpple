bc31dce5af72c8c47abb68017b70ab12c5ea3f23
sunhwa <sumz.ko@gmail.com>
2023-04-28 15:13:06 +0900

    [FEAT] RegisterProduct 페이지

---
+++ client/src/pages/RegisterProduct.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { z } from 'zod';
+import styled from '@emotion/styled';
+import { Button, Container } from '@mantine/core';
+import { useController, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { registerProduct } from '../api/profile';
+import { PROFILE_PATH } from '../routes/routePaths';
+import { SelectProduct, SelectedProduct } from '../components';
+import useToast from '../hooks/useToast';
+
+const Title = styled.h1`
+  color: var(--font-color);
+  text-align: center;
+  font-size: 3.5rem;
+  padding: 50px 0;
+`;
+
+const registerProductScheme = z
+  .object({
+    // color: z.string(), // TODO:
+    productType: z.string().regex(/.+/),
+  })
+  .required();
+
+const RegisterProduct = () => {
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isDirty, isValid, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(registerProductScheme),
+  });
+
+  const {
+    field: { value: selectedProductType, onChange },
+  } = useController({ name: 'productType', control, defaultValue: '' });
+
+  const onSubmit = async data => {
+    try {
+      await registerProduct(data);
+      toast.create({ message: '기기 등록이 완료되었습니다.' });
+      navigate(PROFILE_PATH);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <Container>
+      <Title>기기 등록</Title>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <SelectProduct selectedProductType={selectedProductType} onChange={onChange} />
+
+        <SelectedProduct selectedProductType={selectedProductType} />
+
+        <Container align="center">
+          <Button type="submit" size="lg" w="100%" disabled={!isDirty && !isValid} loading={isSubmitting}>
+            등록하기
+          </Button>
+        </Container>
+      </form>
+    </Container>
+  );
+};
+
+export default RegisterProduct;
