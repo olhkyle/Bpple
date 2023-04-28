18597df942031f05f9a2f61844777b6911674604
olhkyle <kylekwon96@gmail.com>
2023-04-28 21:51:53 +0900

    [FEAT] : CommunityCategory 페이지 컴포넌트 구현 및 SideFilter 컴포넌트 연결

--- client/src/components/community/SideFilter.jsx
+++ client/src/components/community/SideFilter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, Box, NavLink, Stack } from '@mantine/core';
 import styled from '@emotion/styled';
+import FILTERS from '../../constants/filters';
 
 const Container = styled(Box)`
   --spacing: ${({ open }) => (open ? 40 : 0)};
@@ -13,18 +14,26 @@ const Container = styled(Box)`
   padding-right: calc(var(--spacing) * 1px);
   transform: translate3d(calc(var(--x) * -1px), 0, 0);
   transition: all 0.2s;
-  border-right: 1px solid var(--opacity-border-color);
+  border-right: 1px solid var(--footer-font-color);
+
+  button {
+    color: var(--font-color);
+    border-radius: 10px;
+    &:hover {
+      background-color: var(--opacity-bg-color);
+    }
+  }
 `;
 
-const SideFilter = ({ open }) => (
+const SideFilter = ({ open, setCurrentFilter }) => (
   <Container open={open}>
     <Text fz="20px" fw="600">
       유형
     </Text>
     <Stack my="xl" spacing="xl">
-      <NavLink label="해결된 질문" />
-      <NavLink label="FineApple 권장 답변" />
-      <NavLink label="해결되지 않은 질문" />
+      <NavLink label="해결된 질문" onClick={() => setCurrentFilter(FILTERS.completed)} />
+      <NavLink label="FineApple 권장 답변" onClick={() => setCurrentFilter(FILTERS.certified)} />
+      <NavLink label="해결되지 않은 질문" onClick={() => setCurrentFilter(FILTERS.active)} />
     </Stack>
   </Container>
 );
---
+++ client/src/pages/CommunityCategory.jsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link, useParams } from 'react-router-dom';
+import { BsArrowUpRightSquare } from 'react-icons/bs';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useDisclosure } from '@mantine/hooks';
+import { Burger, Container, Divider, Flex, Image, List, Text, Title } from '@mantine/core';
+import { FiArrowRight } from 'react-icons/fi';
+import { COMMUNITY_PATH } from '../routes/routePaths';
+import { AutoComplete, EmptyPostIndicator, PostItem, ShowMoreButton, SideFilter } from '../components/community';
+import { getPostsByCategory, getSearchedPosts } from '../api/posts';
+import FILTERS from '../constants/filters';
+
+const Wrapper = styled(Container)`
+  min-width: 1024px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 1rem;
+  margin-bottom: 5rem;
+  font-size: 0.75rem;
+  color: var(--font-color);
+`;
+
+const PostsContainer = styled(Flex)`
+  margin-top: 1rem;
+  width: 100%;
+  overflow: hidden;
+`;
+
+const CategoryPosts = styled(List)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 3rem;
+  width: 100%;
+`;
+
+const CATEGORY = {
+  iphone: 'iPhone',
+  ipad: 'iPad',
+  mac: 'Mac',
+};
+
+const staleTime = 3000;
+
+const categoryQuery = category => ({
+  queryKey: ['category', category],
+  queryFn: async ({ pageParam = 1 }) => {
+    const { data } = await getPostsByCategory({ param: category, pageParam });
+    return data;
+  },
+  getNextPageParam: (lastPage, allPages) => {
+    const nextPage = allPages.length + 1;
+
+    const { totalLength } = lastPage;
+    return totalLength === 0 || Math.ceil(totalLength / 10) === allPages.length ? undefined : nextPage;
+  },
+  staleTime,
+  select: data => ({
+    posts: data.pages.map(({ posts }) => posts).flat(),
+    totalLength: data.pages[0].totalLength,
+  }),
+});
+
+export const communityCategoryLoader =
+  queryClient =>
+  async ({ params }) => {
+    const query = categoryQuery(params.category);
+
+    // eslint-disable-next-line no-return-await
+    return queryClient.getQueryData(query.queryKey) ?? (await queryClient.fetchInfiniteQuery(query));
+  };
+
+const CommunityCategory = () => {
+  const { category } = useParams();
+
+  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } = useInfiniteQuery(categoryQuery(category));
+
+  const [opened, { toggle }] = useDisclosure(false);
+  const [currentFilter, setCurrentFilter] = React.useState(FILTERS.all);
+
+  const filteredPosts =
+    data?.posts.filter(post =>
+      currentFilter === FILTERS.active
+        ? !post.completed
+        : currentFilter === FILTERS.completed
+        ? post.completed
+        : currentFilter === FILTERS.certified
+        ? post.certified
+        : true
+    ) ?? [];
+
+  return (
+    <Wrapper>
+      <Flex>
+        <Link to={`${COMMUNITY_PATH}`}>
+          <Flex gap="5px" align="center" mr="5px" fz="15px" fw="600" td="none" c="var(--font-color)">
+            <Text>Community</Text>
+            <FiArrowRight />
+          </Flex>
+        </Link>
+        <Link to={`${COMMUNITY_PATH}/${category}`}>
+          <Flex gap="5px" align="center" fz="15px" fw="600" td="none" c="var(--font-color)">
+            <Text>{CATEGORY[category]}</Text>
+            <BsArrowUpRightSquare />
+          </Flex>
+        </Link>
+      </Flex>
+      <Flex gap="0.5rem" mt="2rem">
+        <Image
+          miw={240}
+          width={category !== 'mac' ? '180px' : '220px'}
+          src={`/community/${category}/${category}-category.png`}
+          alt={`${category}-category-img`}
+          fit="cover"
+        />
+        <Flex direction="column" justify="center">
+          <Title mb="3rem">{CATEGORY[category]}</Title>
+          <AutoComplete width={720} queryFn={getSearchedPosts} category={category} />
+        </Flex>
+      </Flex>
+
+      <Flex gap="10px" mt="6rem" mb="1rem" align="center" fw="600">
+        <Text fz="2rem" fw="600" mt="1px">
+          질문
+        </Text>
+        <Text c="blue" fz="2.5rem">
+          {data?.totalLength}
+        </Text>
+      </Flex>
+
+      <Divider mb="1rem" variant="dashed" />
+      <Flex pos="relative">
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          pos="absolute"
+          top="0.5rem"
+          left="0"
+          color="var(--font-color)"
+          aria-label={opened ? 'Close' : 'Open'}
+        />
+        <PostsContainer>
+          <SideFilter open={opened} setCurrentFilter={setCurrentFilter} />
+          {filteredPosts.length === 0 ? (
+            <EmptyPostIndicator />
+          ) : (
+            <CategoryPosts>
+              {filteredPosts.map(post => (
+                <PostItem key={post.id} post={post} />
+              ))}
+            </CategoryPosts>
+          )}
+        </PostsContainer>
+      </Flex>
+      {hasNextPage && <ShowMoreButton loading={isFetchingNextPage} onClick={fetchNextPage} />}
+    </Wrapper>
+  );
+};
+
+export default CommunityCategory;
