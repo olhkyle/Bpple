69143a2cd972cffd181b3f17af0998926c7b51cd
olhkyle <kylekwon96@gmail.com>
2023-04-28 21:48:37 +0900

    [REFAC] : CommunityMe 페이지 관련 컴포넌트 리팩토링 및 작성글 없을 경우 UI 추가

--- client/src/components/community/CommunityMyPosts.jsx
+++ client/src/components/community/CommunityMyPosts.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styled from '@emotion/styled';
 import Recoil from 'recoil';
 import { useQuery } from '@tanstack/react-query';
-import { List } from '@mantine/core';
+import { Chip, Flex, Group, List } from '@mantine/core';
 import userState from '../../recoil/atoms/userState';
 import { communityMeQuery } from '../../pages/CommunityMe';
-import PostItem from './PostItem';
+import { EmptyPostIndicator, PostItem } from '.';
 
 const MyPosts = styled(List)`
   display: flex;
@@ -14,20 +14,38 @@ const MyPosts = styled(List)`
   margin-top: 3rem;
 `;
 
-const CommunityMyPosts = ({ currentFilter }) => {
+const CommunityMyPosts = () => {
   const loginUser = Recoil.useRecoilValue(userState);
+  // todo : loader 관련 useLoaderData hook 사용 예정
   const { data } = useQuery(communityMeQuery(loginUser.email));
+  const [currentFilter, setCurrentFilter] = React.useState('recent');
 
   const filteredPosts = [...data.posts].sort((a, b) =>
     currentFilter === 'recent' ? new Date(b.createAt) - new Date(a.createAt) : new Date(a.createAt - b.createAt)
   );
 
   return (
-    <MyPosts>
-      {filteredPosts.map(post => (
-        <PostItem key={post.title} post={post} />
-      ))}
-    </MyPosts>
+    <>
+      {filteredPosts.length === 0 ? (
+        <EmptyPostIndicator />
+      ) : (
+        <>
+          <Flex justify="flex-end">
+            <Chip.Group value={currentFilter} onChange={setCurrentFilter}>
+              <Group position="center">
+                <Chip value="recent">최신 순</Chip>
+                <Chip value="old">오래된 순</Chip>
+              </Group>
+            </Chip.Group>
+          </Flex>
+          <MyPosts>
+            {filteredPosts.map(post => (
+              <PostItem key={post.title} post={post} />
+            ))}
+          </MyPosts>
+        </>
+      )}
+    </>
   );
 };
 
