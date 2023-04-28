423b52355684033f777644ae6a2f46f16477b099
sunhwa <sumz.ko@gmail.com>
2023-04-28 21:06:13 +0900

    [FEAT] SelectProductRadio 컴포넌트

---
+++ client/src/components/community/SelectProductRadio.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import styled from '@emotion/styled';
+import { Accordion, Container, Radio, Title } from '@mantine/core';
+import { profileQuery } from '../../pages/Profile';
+import MyProductListPanel from './MyProductListPanel';
+import { SelectProductAccordion } from '../common';
+
+const PanelContainer = styled(Container)`
+  width: 100%;
+  color: var(--font-color);
+  padding: 10px;
+  margin-top: 30px;
+
+  .mantine-Accordion-item,
+  .mantine-Accordion-itemTitle,
+  .mantine-Accordion-control {
+    background: none;
+    border-color: var(--font-color);
+  }
+
+  .mantine-Accordion-chevron {
+    color: var(--font-color);
+  }
+`;
+
+const SelectProductRadio = ({ value, onChange, onSelectProduct }) => {
+  const { data: userInfo } = useQuery({ ...profileQuery(), suspense: false });
+
+  return (
+    <PanelContainer>
+      <Radio.Group value={value} onChange={onChange}>
+        <Accordion variant="default">
+          <Accordion.Item value="나의 기기 목록">
+            <Accordion.Control>
+              <Title size="lg" c={'var(--font-color)'}>
+                나의 기기 목록
+              </Title>
+            </Accordion.Control>
+            <MyProductListPanel
+              products={userInfo?.products ?? []}
+              selectedProductType={value}
+              onSelectProduct={onSelectProduct}
+            />
+          </Accordion.Item>
+
+          <Accordion.Item value="제품 목록">
+            <Accordion.Control>
+              <Title size="lg" c={'var(--font-color)'}>
+                제품 목록
+              </Title>
+            </Accordion.Control>
+            <Accordion.Panel>
+              <SelectProductAccordion selectedProductType={value} onSelectProduct={onSelectProduct} />
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </Radio.Group>
+    </PanelContainer>
+  );
+};
+
+export default SelectProductRadio;
