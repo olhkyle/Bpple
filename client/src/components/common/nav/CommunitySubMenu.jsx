9c8a7ceb01098cdbf8365d89b9ae6ff1c3db367d
sunhwa <sumz.ko@gmail.com>
2023-04-30 16:07:02 +0900

    [FEAT] Nav - CommunitySubMenu - Faq 메뉴 추가

--- client/src/components/common/nav/CommunitySubMenu.jsx
+++ client/src/components/common/nav/CommunitySubMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   COMMUNITY_CATEGORY_IPHONE_PATH,
   COMMUNITY_CATEGORY_MAC_PATH,
+  COMMUNITY_FAQ_PATH,
   COMMUNITY_ME_PATH,
   COMMUNITY_PATH,
   COMMUNITY_QUESTION_PATH,
@@ -19,6 +20,7 @@ const CommunitySubMenu = () => {
 
   const rightMenuItems = [
     { size: 'sm', content: '질문하기', path: COMMUNITY_QUESTION_PATH },
+    { size: 'sm', content: '자주 묻는 질문', path: COMMUNITY_FAQ_PATH },
     { size: 'sm', content: '내가 작성한 글 목록', path: COMMUNITY_ME_PATH },
     { size: 'sm', content: '랭킹', path: COMMUNITY_RANK_PATH },
   ];
