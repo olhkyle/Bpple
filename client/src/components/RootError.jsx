f4c71d5f600430027f35d1a0a988a6e63adb1ce5
olhkyle <kylekwon96@gmail.com>
2023-05-01 20:58:57 +0900

    [FEAT] : user 상태 업데이트

--- client/src/components/RootError.jsx
+++ client/src/components/RootError.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Recoil from 'recoil';
 import { Navigate, useRouteError } from 'react-router-dom';
 import { SIGNIN_PATH } from '../routes/routePaths';
+import userState from '../recoil/atoms/userState';
 
 const RootError = () => {
   const error = useRouteError();
+  const setUser = Recoil.useSetRecoilState(userState);
 
   if (error.response.status === 403) {
+    setUser(null);
     return <Navigate to={SIGNIN_PATH} />;
   }
 
