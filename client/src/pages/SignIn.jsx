cd4577c63cbef64fd0b96126ef2bff00554917d7
olhkyle <kylekwon96@gmail.com>
2023-05-01 20:43:04 +0900

    [-] : SignIn 컴포넌트명 오탈자 수정

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,21 +4,22 @@ import { Link } from 'react-router-dom';
 import { Title, Text, Flex } from '@mantine/core';
 import { WiDirectionUpRight } from 'react-icons/wi';
 import { SignInForm } from '../components/auth';
+import { SIGNUP_PATH } from '../routes/routePaths';
 
 const LinkBox = styled.span`
   display: inline-block;
   transform: translateY(8px);
 `;
 
-const SingIn = () => (
-  <Flex py="48px" c="var(--font-color)" direction="column" align="center" gap="8px">
+const SignIn = () => (
+  <Flex py="48px" c="var(--font-color)" direction="column" align="center" gap="8px" h="100vh">
     <Title fz="28px" mb="24px" order={2}>
       FineApple Store에 로그인하세요
     </Title>
     <SignInForm />
     <Text fz="14px">
       FineApple ID가 없으십니까?
-      <Link to="/signup">
+      <Link to={SIGNUP_PATH}>
         지금 만드세요.
         <LinkBox>
           <WiDirectionUpRight size="24" />
@@ -28,4 +29,4 @@ const SingIn = () => (
   </Flex>
 );
 
-export default SingIn;
+export default SignIn;
