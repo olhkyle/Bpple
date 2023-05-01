93c4713b2d6ce11c3fcd3943ffe2fe5f03f9a22f
sunhwa <sumz.ko@gmail.com>
2023-05-01 19:25:57 +0900

    [-] index 추가 - communityProfileLoader

--- client/src/loader/index.js
+++ client/src/loader/index.js
@@ -1,5 +1,6 @@
 export { default as communityCategoryLoader } from './communityCategoryLoader';
 export { default as communityPostLoader } from './communityPostLoader';
 export { default as communityMeLoader } from './communityMeLoader';
+export { default as communityProfileLoader } from './communityProfileLoader';
 export { default as rankLoader } from './rankLoader';
 export { default as profileLoader } from './profileLoader';
