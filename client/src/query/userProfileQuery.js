365e69dd2b1903fa14077cdb486536e0311cd7f6
sunhwa <sumz.ko@gmail.com>
2023-05-01 15:56:56 +0900

    [FIX] userProfileQuery 추가

---
+++ client/src/query/userProfileQuery.js
@@ -0,0 +1,15 @@
+import { fetchUserProfile } from '../api/profile';
+
+const staleTime = 3000;
+
+const userProfileQuery = nickName => ({
+  queryKey: ['profile', nickName],
+  queryFn: async () => {
+    const { data } = await fetchUserProfile(nickName);
+    return data;
+  },
+  staleTime,
+  suspense: true,
+});
+
+export default userProfileQuery;
