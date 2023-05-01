178d9ee00fa11a3a326347d304fcb147ebce4dfc
sunhwa <sumz.ko@gmail.com>
2023-05-01 15:57:10 +0900

    [-] query index 추가

--- client/src/query/index.js
+++ client/src/query/index.js
@@ -3,3 +3,5 @@ export { default as communityPostQuery } from './communityPostQuery';
 export { default as communityMeQuery } from './communityMeQuery';
 export { default as rankQuery } from './rankQuery';
 export { default as profileQuery } from './profileQuery';
+export { default as userPostsQuery } from './userPostsQuery';
+export { default as userProfileQuery } from './userProfileQuery';
