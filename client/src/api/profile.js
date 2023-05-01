fa2f9374fa86b180e6175f732ba1e0ba8caa113f
sunhwa <sumz.ko@gmail.com>
2023-05-01 09:24:47 +0900

    [FEAT] profile - fetchUserProfile API 추가

--- client/src/api/profile.js
+++ client/src/api/profile.js
@@ -6,4 +6,6 @@ const editProfile = userInfo => axios.post('/api/profile/edit', { userInfo });
 
 const registerProduct = productInfo => axios.post('/api/profile/register-product', { productInfo });
 
-export { fetchProfile, editProfile, registerProduct };
+const fetchUserProfile = nickName => axios.get(`/api/profile/community/${nickName}`);
+
+export { fetchProfile, editProfile, registerProduct, fetchUserProfile };
