c725587d2cc85050c8112a8acb5a8d13332c27f0
sunhwa <sumz.ko@gmail.com>
2023-05-01 15:55:10 +0900

    [FIX] queryFn 파일로 분리

--- client/src/pages/CommunityProfile.jsx
+++ client/src/pages/CommunityProfile.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { Container, Skeleton } from '@mantine/core';
 import styled from '@emotion/styled';
 import { useParams } from 'react-router-dom';
-import { fetchUserProfile } from '../api/profile';
-import AvatarProfileInfo from '../components/profile/AvatarProfileInfo';
-import { getUserPosts } from '../api/posts';
-import CommunityCategoryPosts from '../components/community/CommunityCategoryPosts';
+import { userPostsQuery } from '../query';
+import { AvatarProfileInfo, CommunityCategoryPosts } from '../components';
 
 const Wrapper = styled(Container)`
   min-width: 1024px;
@@ -17,32 +15,6 @@ const Wrapper = styled(Container)`
   font-size: 0.75rem;
   color: var(--font-color);
 `;
-
-const staleTime = 3000;
-
-const userProfileQuery = nickName => ({
-  queryKey: ['profile', nickName],
-  queryFn: async () => {
-    const { data } = await fetchUserProfile(nickName);
-    return data;
-  },
-  staleTime,
-  suspense: true,
-});
-
-const userPostsQuery = nickName => ({
-  queryKey: ['posts', nickName],
-  queryFn: async () => {
-    const { data } = await getUserPosts(nickName);
-    return data;
-  },
-  staleTime,
-  suspense: true,
-  select: data => ({
-    posts: data.pages.map(({ posts }) => posts).flat(),
-    totalLength: data.pages[0].totalLength,
-  }),
-});
 
 const CommunityProfile = () => {
   const { nickName } = useParams();
@@ -59,5 +31,4 @@ const CommunityProfile = () => {
   );
 };
 
-export { userProfileQuery };
 export default CommunityProfile;
