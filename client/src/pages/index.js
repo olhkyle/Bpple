8fc251daf08e5f4913538b7fe86519ec05797a5d
olhkyle <kylekwon96@gmail.com>
2023-05-01 10:35:20 +0900

    [FEAT] : NotFound 페이지 추가

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,7 @@
 export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
 export { default as Community } from './Community';
+export { default as CommunityMain } from './CommunityMain';
 export { default as CommunityMe } from './CommunityMe';
 export { default as Profile } from './Profile';
 export { default as ProfileEdit } from './ProfileEdit';
@@ -8,3 +9,4 @@ export { default as RegisterProduct } from './RegisterProduct';
 export { default as CommunityPost } from './CommunityPost';
 export { default as Question } from './Question';
 export { default as Rank } from './Rank';
+export { default as NotFound } from './NotFound';
