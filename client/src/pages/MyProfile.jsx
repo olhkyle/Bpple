0dd008ca94a0c2019da933dac67e4b20dccf8a5b
sunhwa <sumz.ko@gmail.com>
2023-05-02 15:29:42 +0900

    [FIX] MyProfile - loader 적용 및 Suspense 제거

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -3,7 +3,6 @@ import Recoil from 'recoil';
 import styled from '@emotion/styled';
 import userState from '../recoil/atoms/userState';
 import { MyProfile } from '../components';
-import { Loader } from '../components/common';
 
 const Title = styled.h1`
   color: var(--font-color);
@@ -18,10 +17,7 @@ const Profile = () => {
   return (
     <>
       <Title>{loginUser.nickName}님, 안녕하세요.</Title>
-
-      <React.Suspense fallback={<Loader />}>
-        <MyProfile />
-      </React.Suspense>
+      <MyProfile />
     </>
   );
 };
