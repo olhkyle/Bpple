8d37dffb4454a77e24721bed6332f3514f89caa3
sunhwa <sumz.ko@gmail.com>
2023-05-12 14:08:01 +0900

    [FEAT] dotenv

--- client/firebase/app.js
+++ client/firebase/app.js
@@ -3,14 +3,16 @@ import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const { VITE_FIREBASE_API_KEY, VITE_APP_ID } = import.meta.env;
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCpGcwkoqAfk_f9-U-UVtdRQcAyCcwYykc',
+  apiKey: VITE_FIREBASE_API_KEY,
   authDomain: 'fineapple-ea261.firebaseapp.com',
   projectId: 'fineapple-ea261',
   storageBucket: 'fineapple-ea261.appspot.com',
   messagingSenderId: '240412251713',
-  appId: '1:240412251713:web:e624da67373418c1cf58c0',
+  appId: VITE_APP_ID,
 };
 
 // Initialize Firebase
